9254b55e0d8d9f96afed40cf733fd6df83087e8a
lenovo <black03139090313@gmail.com>
2023-07-20 11:32:25 +0500

    13

--- src/pages/Users/addUser.jsx
+++ src/pages/Users/addUser.jsx
@@ -1,9 +1,44 @@
-function AddUser() {
+import "./addUser.css";
+import { connect } from "react-redux";
+import { useEffect } from "react";
+// eslint-disable-next-line import/named
+import userReducer, { addUser, deleteUser, editUser, getUsers } from "../../reducer/userReducer.js";
+
+function AddUser({ usersDataReducer }) {
+  useEffect(() => {
+  }, []);
+
   return (
-    <div className="text-center">
-      <h4>gkjasdfha askjdhaksjd askjdashd</h4>
+    <div>
+      <h3 className="text-center">Xodim qo`shish</h3>
+      <div className="mt-3 ">
+        {/* eslint-disable-next-line react/button-has-type */}
+        <button className="justify-end rounded hover:rounded-lg btnAdd">Xodim qo`shish</button>
+      </div>
+
+      <div className="mt-4">
+        <table className="table-fixed border-2 table-row">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Name</th>
+              <th>Name</th>
+              <th>Name</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>dasd</td>
+              <td>dasd</td>
+              <td>dasd</td>
+              <td>dasd</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
 
-export default AddUser;
+export default connect((userReducer), { getUsers, addUser, editUser, deleteUser })(AddUser);
